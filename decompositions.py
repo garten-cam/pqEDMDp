f355a31a03554760d727675546551b72d6583bf9
Camilo Garcia <camilo_7@msn.com>
2023-06-14 08:56:45 +0200

    Fix working with inputs

--- decompositions.py
+++ decompositions.py
@@ -5,15 +5,16 @@ returns a decomposition object
 import numpy as np
 from scipy.optimize import minimize
 from functools import partial
-from sympy.polys import orthopolys as op
 from sympy import symbols, linear_eq_to_matrix, Matrix, linsolve, lambdify
+
+
 class decomposition(object):
     def __init__(
             self,
             observable,
             xtr,
             ytr,
-            ):
+    ):
         # Generic class for defining a decomposition
         self.observable = observable
         self._U = self._set_U(xtr, ytr)
@@ -21,21 +22,23 @@ class decomposition(object):
     @property
     def U(self):
         return self._U
+
     def _set_U(self, xtr, ytr):
         g = self.g(xtr)
         a = self.a(xtr, ytr)
         # with the g and a matrices, get the transpose of u
-        return np.matmul(np.linalg.inv(g),a)
-    
+        return np.matmul(np.linalg.inv(g), a)
+
     @property
     def A(self):
         # Extrat the A matrix from U; x=Ax
         if not self.observable.nU:
             a = np.transpose(self.U)
         else:
-            a = np.transpose(self.U[:-self.observable.nU - 1, 
-                      :-self.observable.nU - 1])
+            a = np.transpose(self.U[:-self.observable.nU - 1,
+                                    :-self.observable.nU - 1])
         return a
+
     @property
     def B(self):
         # extract the B matrix from U; x=Ax+Bu
@@ -43,7 +46,8 @@ class decomposition(object):
             b = None
         else:
             b = self.U[-self.observable.nU:, :-self.observable.nU - 1]
-        return b 
+        return b
+
     @property
     def C(self):
         # this one is complicated, I must do all the inverse of the
@@ -51,47 +55,51 @@ class decomposition(object):
         # x = QR, ro recover R because all the data lives in Q now
         # R = np.linalg.inv(self.observable.r_trx)
         # Given the ordering of the polynomials, we need the additive inverse
-        # of the first nSV terms 
-        order_one_obs = Matrix(self.observable.poly_prod[:self.observable.nSV + 1])
+        # of the first nSV terms
+        order_one_obs = Matrix(
+            self.observable.poly_prod[:self.observable.nSV + 1])
         # order_one_obs[0] = symbols('1')
         # we also need a dummy variable for the representing the functions
         # As many dummy variables as there are states in the system
-        z = Matrix([symbols('1'),*symbols(f'z:{self.observable.nSV}')])
+        z = Matrix([symbols('1'), *symbols(f'z:{self.observable.nSV}')])
         # define again the variables
         x = symbols(f'x:{self.observable.nSV}')
         # Equation system to solve
         f_to_inv = order_one_obs - z
         # Convert to a system of equations
-        A, b = linear_eq_to_matrix(list(f_to_inv), [symbols('1'),*x])
+        A, b = linear_eq_to_matrix(list(f_to_inv), [symbols('1'), *x])
         # solve the system
-        sol = linsolve((A,b),[symbols('1'),*x])
+        sol = linsolve((A, b), [symbols('1'), *x])
         # get the new matrices for A and b
-        Az, _ = linear_eq_to_matrix(list(sol)[0],
-                            [symbols('1'),*symbols(f'z:{self.observable.nSV}')])
+        Az, _ = linear_eq_to_matrix(
+            list(sol)[0],
+            [symbols('1'), *symbols(f'z:{self.observable.nSV}')])
         # Remove the constant term
-        Az = Az[1:,:]
+        Az = Az[1:, :]
         # Add the constant term in case b = number +- z
-        Az[:,0] = Matrix(list(linsolve((b[1:]),
-                                symbols(f'z:{self.observable.nSV}')))).T
+        Az[:, 0] = Matrix(
+            list(linsolve((b[1:]),
+                          symbols(f'z:{self.observable.nSV}')))).T
         # create the c matrix
-        c = np.zeros(self.observable.pq_matrix.shape)
+        c = np.zeros((self.observable.nSV,
+                      self.observable.pq_matrix.shape[1]))
         # Assign the c entries
-        c[:,:self.observable.nSV + 1] = Az
+        c[:, :self.observable.nSV + 1] = Az
         return c
 
     def g(self, xtr):
         # I am going to divide the "g" and "a" matrix calcualtion.
         # That way, I can apply all the necessary calculations on "g"
-        # and the observables. For orthogonalization and polynomial 
+        # and the observables. For orthogonalization and polynomial
         # manipulation purposes
         # Get the observables function
         obs = self.observable
         # Evaluate the training data in x and y
         x_eval = self.xy_eval(obs, xtr)
-        # With the QR decomposition I can orthogonalize the evaluations and 
-        # get the well conditioned g matrix. 
+        # With the QR decomposition I can orthogonalize the evaluations and
+        # get the well conditioned g matrix.
         # Get the orthogonal x_eval
-        obs.r_matrix = np.linalg.qr(x_eval)[1] # gets the second output
+        obs.r_matrix = np.linalg.qr(x_eval)[1]  # gets the second output
         # # Assign the orthogonalization matrix
         obs.r_trx = np.linalg.inv(obs.r_matrix)
         # # evaluate again
@@ -99,27 +107,27 @@ class decomposition(object):
         g = np.matmul(x_eval.transpose(), x_eval)/x_eval.shape[0]
         # It is a good idea to orthogonalize here, always.
 
-        return g 
+        return g
 
     def a(self, xtr, ytr):
-        # The same as g but with a: x'y 
+        # The same as g but with a: x'y
         obs = self.observable
         x_eval = self.xy_eval(obs, xtr)
         y_eval = self.xy_eval(obs, ytr)
         a = np.matmul(x_eval.transpose(), y_eval)/x_eval.shape[0]
         return a
 
-
     @staticmethod
     def is_invertible(A):
         return A.shape[0] == A.shape[1] and np.linalg.matrix_rank(A) == A.shape[0]
-    
+
     @staticmethod
     def xy_eval(obs, xtr):
         psi = obs.pq_function
-        return np.concatenate((np.ones((xtr.shape[0],1))*obs.r_trx[0][0],
-                                np.array(psi(*xtr.transpose())).transpose()),
-                                axis=1)
+        return np.concatenate((np.ones((xtr.shape[0], 1))*obs.r_trx[0][0],
+                               np.array(psi(*xtr.transpose())).transpose()),
+                              axis=1)
+
 
 class maxLikeDecomp(decomposition):
     def __init__(self, observable, xtr, ytr):
@@ -132,30 +140,33 @@ class maxLikeDecomp(decomposition):
     @property
     def U0(self):
         return self._U0
+
     def _set_U0(self, xtr, ytr):
         return decomposition(self.observable, xtr, ytr).U
 
     @property
     def Q(self):
         return self._Q
+
     def _set_Q(self, xtr, ytr):
-        obs = self.observable # save in its own variable, makes the
+        obs = self.observable  # save in its own variable, makes the
         # code more readable
         # evaluate the polybase
         x_eval = self.xy_eval(obs, xtr)
         # Covariance calculation
-        ## mean value per column
+        # mean value per column
         x_mean = np.mean(x_eval, axis=0)
-        ## q matrix
-        return np.matmul((x_eval - x_mean).transpose(), (x_eval - x_mean))/\
+        # q matrix
+        return np.matmul((x_eval - x_mean).transpose(), (x_eval - x_mean)) /\
             (x_eval.shape[0] - 1)
 
     @property
     def U(self):
         return self._U
+
     def _set_U(self, xtr, ytr):
         # save the observable function. Not necesssary, but ok...
-        obs = self.observable # save in its own variable, makes the
+        obs = self.observable  # save in its own variable, makes the
         # code more readable
         # evaluate the polybase
         x_eval = self.xy_eval(obs, xtr)
@@ -163,19 +174,20 @@ class maxLikeDecomp(decomposition):
         # Now we are ready for the optimization
         # number of observables to fit
         n_obs = self.observable.pq_matrix.shape[1]
-        u = np.identity(self._U0.shape[0]) # Preallocate
+        u = np.identity(self._U0.shape[0])  # Preallocate
         for u_column in range(1, n_obs):
-            # turn the objective function into a new function dependent only on 
+            # turn the objective function into a new function dependent only on
             # one parameter
-            obj_f = partial(maxLikeDecomp.cost_function, 
-                          Q = self.Q,
-                          sigma = self.Q[u_column, u_column],
-                          x_eval = x_eval,
-                          y_eval = y_eval[:, u_column])
-            u[:,u_column] = minimize(obj_f, # New objective function f(u_col)
-                                     self._U0[:,u_column] - 1, # Initial condition
-                                     method = 'BFGS',
-                                     options = {'disp': False}).x
+            obj_f = partial(maxLikeDecomp.cost_function,
+                            Q=self.Q,
+                            sigma=self.Q[u_column, u_column],
+                            x_eval=x_eval,
+                            y_eval=y_eval[:, u_column])
+            u[:, u_column] = minimize(obj_f,  # New objective function f(u_col)
+                                      self._U0[:, u_column] - \
+                                      1,  # Initial condition
+                                      method='BFGS',
+                                      options={'disp': False}).x
 
         return u
 
@@ -192,17 +204,14 @@ class maxLikeDecomp(decomposition):
 
     @staticmethod
     def cost_function(u_col, Q, sigma, x_eval, y_eval):
-        j = np.sum((y_eval - np.array(np.matmul(x_eval,
-                                    u_col)))**2/(sigma**2 + 
-                                            np.matmul(np.matmul(u_col, Q), 
-                                                np.atleast_2d(u_col).T)))
+        j = np.sum((
+            y_eval - np.array(
+                np.matmul(x_eval,
+                          u_col))
+        )**2/(sigma**2 +
+              np.matmul(np.matmul(u_col, Q),
+                        np.atleast_2d(u_col).T)))
         return j
 
-if __name__ == "__main__":
-    import scipy.integrate as spi
-    # I need a dummy dataset in at least two variables
-    def duffODE(t, x):
-        return [x[1], -0.5*x[1] - x[0] - x[0]**2]
-    # define the tspan
 
-    print(1)
+# if __name__ == "__main__":
--- pqEDMD.py
+++ pqEDMD.py
@@ -29,7 +29,8 @@ class pqEDMD:
         self.method = method
         self.poly_param = poly_param
         self.normalization = normalization
-        self.scalers = {'xscaler' : None}  # I have no idea if this is good practice, but I do 
+        # I have no idea if this is good practice, but I do
+        self.scalers = {'xscaler': None}
         # not like having attributes scattered around the code without being
         # defined in the constructor
         # This will return an array of unique solutions
@@ -56,57 +57,61 @@ class pqEDMD:
                                           self.p,
                                           self.q)
         # That nique obs list was hard...
-        # Coninue, I need to create the matrices for the evaluation of the 
+        # Coninue, I need to create the matrices for the evaluation of the
         # observalbes.
         # Return the scalers as an attribute
-        xtr, ytr, self.scalers = pqEDMD.snapshots(training_data, 
+        xtr, ytr, self.scalers = pqEDMD.snapshots(training_data,
                                                   self.normalization)
         # I have the data, next, perform the decomposition
-        # Now, I should call the prefered decomposition for all the unique 
+        # Now, I should call the prefered decomposition for all the unique
         # observables in the observables list
-        decompositions = [[] for _ in range(len(obs_list))] # preallocation
+        decompositions = [[] for _ in range(len(obs_list))]  # preallocation
         for decomposition in range(len(obs_list)):
-            decompositions[decomposition] = getattr(dc, 
-                    f"{self.method}Decomp")(obs_list[decomposition], xtr, ytr)
+            decompositions[decomposition] = getattr(dc,
+                                                    f"{self.method}Decomp")(obs_list[decomposition], xtr, ytr)
         return decompositions
-    
+
     @staticmethod
     def predict(decp, x0, scalers, n_points, u=None):
         # ok, do it for one and then set the for
-        # Get the observable that includes the constant term 
+        # Get the observable that includes the constant term
         ev_fun = decp.evol_function
-        # Get the R matrix in case there is ortogonalization
-        R = np.linalg.inv(decp.observable.r_trx)
         # preallocate
-        pred = [{'sv':np.zeros((n_points,decp.observable.nSV))}\
+        pred = [{'sv': np.zeros((n_points, decp.observable.nSV))}
                 for _ in range(len(x0))]
+        # There are two options, if there is an input or not.
+        # I am creating two sub functions to avoid a huge if-else
         # assign the initial condition
         for sample in range(len(x0)):
-            pred[sample]['sv'][0,:] = x0[sample]
+            pred[sample]['sv'][0, :] = x0[sample]
         # Main loop to assign all the values
         for sample in range(len(x0)):
             for step in range(1, n_points):
-                if scalers['xscaler'] is not None:
-                    xprev = list(scalers['xscaler'].transform(
-                        pred[sample]['sv'][step-1,:].reshape(1, -1)
-                    )[0])
+                # call the predictions here
+                if u is None:
+                    xprev = pqEDMD._xprev(pred[sample]['sv'][step-1, :],
+                                          scalers)
                 else:
-                    xprev = pred[sample]['sv'][step-1,:]
-                
+                    xprev = pqEDMD._xprev_u(pred[sample]['sv'][step-1, :],
+                                            u[sample][step-1, :],
+                                            scalers)
+
                 # apply the necessary tranformations to xprev
                 # 1. evolve
                 xpost = ev_fun(*xprev)
-                # 2. If there was normalization, bring back to the original obs
+                # 2. If there was orthogonalization, bring back
+                # to the original obs
                 xpost_ogn = np.matmul(xpost, decp.observable.r_matrix)
                 # 3. Bring now to the original state space
                 xss = np.transpose(np.matmul(decp.C, np.transpose(xpost_ogn)))
+                # Do not like the double transpose but it is consistent with
+                # the lit
                 # 4. descale if necessary
                 if scalers['xscaler'] is not None:
-                    pred[sample]['sv'][step,:] = \
-                    scalers['xscaler'].inverse_transform(xss)
+                    pred[sample]['sv'][step,
+                                       :] = scalers['xscaler'].inverse_transform(xss)
                 else:
-                    pred[sample]['sv'][step,:] = xss 
-        
+                    pred[sample]['sv'][step, :] = xss
         return pred
 
     @staticmethod
@@ -119,9 +124,9 @@ class pqEDMD:
         obs_inst = getattr(obs, f"{poly_type.lower()}Obs")
         # Do a set comprenhension to store unique elements and turn it
         # into a list
-        obs_list = list({obs_inst(pq[0], pq[1], nsv, nu, param) 
+        obs_list = list({obs_inst(pq[0], pq[1], nsv, nu, param)
                          for pq in product(p, q)})
-        
+
         return obs_list
 
     def snapshots(training_data, normalization=True):
@@ -136,30 +141,33 @@ class pqEDMD:
         # 5. Normalize if it is the case
         x_prev = np.concatenate(  # concatenate all x in the training set
             # from the first until the antepenultimate
-            [training_data[sample]['sv'][:-2,:]
-             for sample in range(len(training_data))],axis=0
+            [training_data[sample]['sv'][:-2, :]
+             for sample in range(len(training_data))], axis=0
         )
         # concatenate all u in the training set
-            # form the first until the antepenultimate
+        # form the first until the antepenultimate
         if 'u' in training_data[0]:
-            u_prev = np.concatenate([training_data[sample]['u'][:-2,:]
-             for sample in range(len(training_data))],axis=0
-        )
-        x_post = np.concatenate([training_data[sample]['sv'][1:-1,:]
-             for sample in range(len(training_data))],axis=0
-        )
+            u_prev = np.concatenate([training_data[sample]['u'][:-2, :]
+                                     for sample in range(len(training_data))],
+                                    axis=0
+                                    )
+        x_post = np.concatenate([training_data[sample]['sv'][1:-1, :]
+                                 for sample in range(len(training_data))],
+                                axis=0
+                                )
         if 'u' in training_data[0]:
-            u_post = np.concatenate([training_data[sample]['u'][1:-1,:]
-             for sample in range(len(training_data))],axis=0
-        )
-        # 
-        # I am going to return a normalizer or scaler for the state and for the 
+            u_post = np.concatenate([training_data[sample]['u'][1:-1, :]
+                                     for sample in range(len(training_data))],
+                                    axis=0
+                                    )
+        #
+        # I am going to return a normalizer or scaler for the state and for the
         # input in different objects, this make the calculation easier at later
         # stages
         # After somen tought, the scaling should be responsibility of the user,
         # before doing anything with the algorithm. But it is coded, so...
         if normalization:
-            scalers = {'xscaler' : preprocessing.MinMaxScaler()}
+            scalers = {'xscaler': preprocessing.MinMaxScaler()}
             if 'u' in training_data[0]:
                 scalers['uscaler'] = preprocessing.MinMaxScaler()
                 xtr = np.concatenate(
@@ -174,23 +182,49 @@ class pqEDMD:
 
         else:
             if 'u' in training_data[0]:
-                xtr = np.concatenate((x_prev, u_prev), axis=1)        
+                xtr = np.concatenate((x_prev, u_prev), axis=1)
                 ytr = np.concatenate((x_post, u_post), axis=1)
             else:
                 xtr = x_prev
                 ytr = x_post
-            scalers = {'xscaler' : None}
-            scalers['uscaler'] =  None
+            scalers = {'xscaler': None}
+            scalers['uscaler'] = None
         return xtr, ytr, scalers
-        
+
+    @staticmethod
+    def _xprev(x_prev, scalers):
+        if scalers['xscaler'] is not None:
+            xprev = list(scalers['xscaler'].transform(
+                x_prev.reshape(1, -1)
+            )[0])
+        else:
+            xprev = x_prev
+        return xprev
+
+    @staticmethod
+    def _xprev_u(x_prev, u, scalers):
+        if scalers['xscaler'] is not None:
+            xprev = list(np.concatenate(
+                scalers['xscaler'].transform(
+                    x_prev.reshape(1, -1),
+                    scalers['uscaler'].transform(
+                        u
+                    ))
+            )[0])
+        else:
+            xprev = list(np.concatenate(
+                (x_prev, u)))
+        return xprev
+
+
 if __name__ == "__main__":
-    # Test the implementation with the toy duffing equation
+    import matplotlib.pyplot as plt
     from scipy.integrate import odeint
 
+    # Test the implementation with the toy duffing equation
     def duffode(x, t):
         # Duffing with two AS points
         return [x[1], -0.5*x[1] + x[0] - x[0]**3]
-
 
     # rng = np.random.default_rng(1342)
     rng = np.random.default_rng(1342)
@@ -203,47 +237,78 @@ if __name__ == "__main__":
     #                  [-0.7907, 0.1553],
     #                  [-1.4130, -0.3232],
     #                  [-1.6306, 0.7409]])
-    # use some random input
-    # u = 2*rng.random((num_ics,1))
-    # The inputs will be a step that is also a random number
-    
-    # I need a dictionary with the necessary data.
-    samples = [{'sv':np.empty((0,0)), 't':np.empty((0,0))}\
-                for sample in range(num_ics)] 
+
     t_end = 30
     n_points = 301
 
-    import matplotlib.pyplot as plt
+    # I need a dictionary with the necessary data.
+    # samples = [{'sv':np.empty((n_points,2)), 't':np.empty((n_points,1))}\
+    #             for _ in range(num_ics)]
+    #
+    # for sample in range(num_ics):
+    #     t = np.linspace(0,t_end,n_points)
+    #     sol = odeint(duffode, ics[sample, :], t)
+    #     samples[sample]['sv'] = sol
+    #     samples[sample]['t'] = t
+    #     # samples[sample]['u'] = np.ones((n_points,1))*u[sample]
+    #     # plt.plot(sol[:,0],sol[:,1])
+    #     # plt.title(f'sample:{sample}')
+    #
+    #
+    duff_EDMD = pqEDMD(p=[3], q=[1],
+                       polynomial='Legendre', normalization=False)
+    # # fit the alg
+    tr = range(0, 2)
+    ts = range(2, num_ics)
+    # duff_decomps = duff_EDMD.fit([samples[i] for i in tr])
+    # # duff_decomps = duff_EDMD.fit(samples[0])
+    # # test with some samples
+    # approximations = duff_EDMD.predict(duff_decomps[0],
+    #                     [samples[i]['sv'][0,:] for i in ts],
+    #                     duff_EDMD.scalers,
+    #                     n_points)
+
+    # plt.figure()
+    # [plt.plot(samples[i]['sv'][:,0],samples[i]['sv'][:,1], 'r') for i in tr]
+    # [plt.plot(samples[i]['sv'][:,0],samples[i]['sv'][:,1], 'b') for i in ts]
+    # [plt.plot(approximations[i]['sv'][:,0],approximations[i]['sv'][:,1], 'k-.') for i in range(len(ts))]
+    # plt.show()
+
+    # Test the code for inputs
+    # use some random input
+    inputs = 100*rng.random((num_ics, 1)) - 50
+    # The inputs will be a step that is also a random number
+
+    def duffode_u(x, t, u):
+        # Duffing with two AS points
+        return [x[1], -0.5*x[1] + x[0] - x[0]**3 + u]
+
+    # preallocate the samples list
+    samples_u = [{'sv': np.empty((n_points, 2)),
+                  't': np.empty((n_points, 1)), 'u': np.empty((n_points, 1))}
+                 for _ in range(num_ics)]
+
+    # populate the samples
     for sample in range(num_ics):
-        t = np.linspace(0,t_end,n_points)
-        sol = odeint(duffode, ics[sample, :], t)
-        samples[sample]['sv'] = sol
-        samples[sample]['t'] = t
-        # samples[sample]['u'] = np.ones((n_points,1))*u[sample]
-        # plt.plot(sol[:,0],sol[:,1])
-        # plt.title(f'sample:{sample}')
-        
+        t = np.linspace(0, t_end, n_points)
+        sol = odeint(duffode_u, ics[sample, :], t, args=(inputs[sample][0],))
+        samples_u[sample]['sv'] = sol
+        samples_u[sample]['t'] = t
+        samples_u[sample]['u'] = np.full((n_points, 1), inputs[sample][0])
 
-    duff_EDMD = pqEDMD(p=[3], q=[1], polynomial='Legendre', normalization=False)
-    # fit the alg
-    tr = range(0,2)
-    ts = range(2,num_ics)
-    duff_decomps = duff_EDMD.fit([samples[i] for i in tr])
-    # duff_decomps = duff_EDMD.fit(samples[0])
-    # test with some samples
-    approximations = duff_EDMD.predict(duff_decomps[0], 
-                        [samples[i]['sv'][0,:] for i in ts],
-                        duff_EDMD.scalers,
-                        n_points)
-    # plt.figure()
-    # [plt.plot(samples[i]['t'],samples[i]['sv']) for i in (1,3,4,5)]
-    # plt.figure()
-    # [plt.plot(samples[i]['t'],approximations[i]['sv']) for i in range(4)]
-    
-    plt.figure()
-    [plt.plot(samples[i]['sv'][:,0],samples[i]['sv'][:,1], 'r') for i in tr]
-    [plt.plot(samples[i]['sv'][:,0],samples[i]['sv'][:,1], 'b') for i in ts]
-    [plt.plot(approximations[i]['sv'][:,0],approximations[i]['sv'][:,1], 'k-.') for i in range(len(ts))]
+    duff_decomps_u = duff_EDMD.fit([samples_u[i] for i in tr])
+
+    app_u = duff_EDMD.predict(duff_decomps_u[0],
+                              [samples_u[i]['sv'][0, :] for i in ts],
+                              duff_EDMD.scalers,
+                              n_points,
+                              [samples_u[i]['u'] for i in ts])
+
+    [plt.plot(samples_u[i]['sv'][:, 0], samples_u[i]['sv'][:, 1], 'r')
+     for i in tr]
+    [plt.plot(samples_u[i]['sv'][:, 0], samples_u[i]['sv'][:, 1], 'b')
+     for i in ts]
+    [plt.plot(app_u[i]['sv'][:, 0], app_u[i]
+              ['sv'][:, 1], 'k-.') for i in range(len(ts))]
     plt.show()
-
     x = 1
