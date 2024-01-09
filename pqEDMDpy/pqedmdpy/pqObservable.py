e7213d522aee7900117d0cb2d695f10a07909a49
Camilo Garcia <camilo_7@msn.com>
2024-01-09 17:08:23 +0100

    huge changes to observables

--- pqEDMDpy/pqedmdpy/pqObservable.py
+++ pqEDMDpy/pqedmdpy/pqObservable.py
@@ -1,290 +1,123 @@
-'''
+"""
 Author: Camilo Garcia Tenorio
 Returns an observable object according to the type of polynomial
 the type and the p, q parameters
-'''
-from sympy.polys import orthopolys as op
-from sympy import symbols, Matrix, zeros, lambdify
-import math
+"""
+
 import numpy as np
+from sympy import Matrix, lambdify, symbols
+from sympy.polys import orthopolys as op
 
 
 class pqObservable:
-    def __init__(self,
-                 p=2,
-                 q=0.8,
-                 nSV=2,
-                 nU=1,
-                 huge_p_matrix=None):
-        self.p = p
-        self.q = q
-        self.nSV = nSV
-        self.nU = nU
-        self.p_matrix = huge_p_matrix
-        self.pq_matrix = None
-        self.r_trx = None
-        self.r_matrix = None
+    def __init__(self, p=2, q=0.8, l=2):  # noqa: E741
+        self.p = p  # max polynomial order
+        self.q = q  # q quasi norm
+        self.l = l  # number of variables to observe
 
-    @property
+    def pq_mat(self):
+        pm = self.p_matrix()  # get the p matrix
+        # reduce according to the q-quasi norm
+        pqm = pm[:, np.linalg.norm(pm, ord=self.q, axis=0) <= self.p]
+        # sort in lexicographical ordering
+        pqm = pqm[:, np.argsort(np.linalg.norm(pqm, axis=0, ord=self.p), kind="stable")]
+        return pqm
+
+    def poly_base(self):
+        x = symbols(f"x:{self.l}")
+        orders = self.pq_mat()
+        o_sym = self.assign_poly(orders, x)
+        poly_b = np.prod(o_sym, axis=0)
+        return poly_b
+
+    def obs_fun(self):
+        x = symbols(f"x:{self.l}")
+        p_b = self.poly_base()
+        return lambdify(x, Matrix(p_b), modules="numpy")
+
     def p_matrix(self):
-        return self._p_matrix
-
-    @p_matrix.setter
-    def p_matrix(self, value):
-        pm = np.zeros((self.nSV, (self.p + 1)**(self.nSV)), dtype=int)
-        if value is not None:
-            self._p_matrix = value
-        elif (self.p**(self.nSV) - 1) > 9.0e+18:
-            raise ValueError("number of state variables and p value\
-                    combination exceeds maximum size")
+        pm = np.zeros((self.l, ((self.p + 1) ** (self.l)) - 1), dtype=int)
+        if (self.p ** (self.l) - 1) > 9.0e18:
+            raise ValueError(
+                "number of state variables and p value\
+                    combination exceeds maximum size"
+            )
         else:
             # preallocate the final matrix
-            for col in range((self.p + 1)**(self.nSV)):
+            for col in range(1, (self.p + 1) ** (self.l)):
                 base_string = np.base_repr(col, self.p + 1)
-                base_string = '0' * (self.nSV - len(base_string)) + base_string
-                pm[:, col] = np.flip([int(x) for x in base_string])
-        # For the qr or orthogonalization parts in the following,
-        # it is better to have the poynomials ordered.
-        # Assume the lexicographical ordering according to the
-        # state variables. i.e., x_1 < x_2 if ||x_1|| < ||x_2||
-        # and, if the norm of two arbitrary polynomials is equal,
-        # the lowest component determines the ordering
-        # [0,1,1]'<[1,1,0]'
-        self._p_matrix = pm[:, np.argsort(np.linalg.norm(
-            pm, axis=0, ord=self.p),
-            kind='stable')]
-        # Add the effect of u
+                base_string = "0" * (self.l - len(base_string)) + base_string
+                pm[:, col - 1] = np.flip([int(x) for x in base_string])
+        return pm
 
-    @property
-    def pq_matrix(self):
-        return self._pq_matrix
-
-    @pq_matrix.setter
-    def pq_matrix(self, value):
-        if value is not None:
-            self._pq_matrix = value
-        else:
-            # trim according to q
-            pqm = self.p_matrix[:, np.linalg.norm(self.p_matrix,
-                                                  ord=self.q,
-                                                  axis=0) <= self.p]
-            # add the rows and columns of the input
-            self._pq_matrix =\
-                np.concatenate(
-                    (np.concatenate((pqm,
-                                     np.zeros([self.nSV, self.nU])),
-                                    axis=1),
-                     np.concatenate((np.zeros([self.nU, pqm.shape[1]]),
-                                     np.eye(self.nU)), axis=1)),
-                    axis=0)
-
-    @property
-    def r_trx(self):
-        return self._r_trx
-
-    @r_trx.setter
-    def r_trx(self, value):
-        # This is to use orthogonalization from the data.
-        # If it is not provided,
-        # Just use an identity matrix for weighting the observables.
-        pq_m_rows = self.pq_matrix.shape[1]
-        if value is not None:
-            # check if the matrix is the tright size
-            if value.shape == (pq_m_rows, pq_m_rows):
-                self._r_trx = value
-        else:
-            self._r_trx = np.identity(pq_m_rows)
-
-    @property
-    def r_matrix(self):
-        return self._r_matrix
-
-    @r_matrix.setter
-    def r_matrix(self, value):
-        # This is to use orthogonalization from the data.
-        # If it is not provided,
-        # Just use an identity matrix for weighting the observables.
-        pq_m_rows = self.pq_matrix.shape[1]
-        if value is not None:
-            # check if the matrix is the right size
-            if value.shape == (pq_m_rows, pq_m_rows):
-                self._r_matrix = value
-        else:
-            self._r_matrix = np.identity(pq_m_rows)
-
-    @property
-    def pq_function(self):
-        x = symbols(f'x:{self.nSV + self.nU}')
-        # Add otrhogonalization, just in case there is a r_trx different than
-        # identity. Don't do it as an if... just do the multiplication
-        poly_prod = Matrix(np.matmul(self.poly_prod, self.r_trx))
-        # lambdify...
-        return lambdify(x, poly_prod[1:], modules='numpy')
+    def assign_poly(self, orders, xsym):
+        return [[x**ord for ord in orders[c]] for c, x in enumerate(xsym)]
 
     def __eq__(self, other):
         equal = False
         # If the shapes are not equal, they are definitely not equal
-        if self.pq_matrix.shape == other.pq_matrix.shape:
+        if self.pq_mat().shape == other.pq_mat().shape:
             # If the shapes are equal then... Compare the whole array
-            equal = (self.pq_matrix == other.pq_matrix).all()
+            equal = (self.pq_mat() == other.pq_mat()).all()
         return equal
 
     def __hash__(self):
         return hash((self.p))
 
-    @property
-    def poly_prod(self):
-        return Matrix()
-
 
 class hermiteObs(pqObservable):
-    @property
-    def poly_prod(self):
-        x = symbols(f'x:{self.nSV + self.nU}')
-        indexes = self.pq_matrix
-        # create a numpy array the same size as the indexes to store
-        # the polynomials
-        polys = zeros(*indexes.shape)
-        if self.nU > 0:
-            for idx, sv in enumerate(x[:-self.nU]):
-                polys[idx, :] = Matrix([op.hermite_poly(power, sv)
-                                        for power
-                                        in indexes[idx, :]]).transpose()
-            for idx, u in enumerate(x[-self.nU:]):
-                # Legendre polys in the first order are the constant function
-                # l^1(x)=x, perfect for the inputs
-                polys[idx + self.nSV, :] =\
-                    Matrix([op.legendre_poly(power, u)
-                            for power
-                            in indexes[idx + self.nSV, :]]).transpose()
-        else:
-            for idx, sv in enumerate(x):
-                polys[idx, :] = Matrix([op.hermite_poly(power, sv)
-                                        for power
-                                        in indexes[idx, :]]).transpose()
-        # take the product per column
-        return Matrix([math.prod(polys[:, col])
-                       for col in range(polys.shape[1])]).transpose()
+    def assign_poly(self, orders, xsym):
+        return [
+            [op.hermite_poly(ord, x) for ord in orders[c]] for c, x in enumerate(xsym)
+        ]
 
 
 class laguerreObs(pqObservable):
-    @property
-    def poly_prod(self):
-        x = symbols(f'x:{self.nSV + self.nU}')
-        indexes = self.pq_matrix
-        # create a numpy array the same size as the indexes to store
-        # the polynomials
-        polys = zeros(*indexes.shape)
-        if self.nU > 0:
-            for idx, sv in enumerate(x[:-self.nU]):
-                polys[idx, :] = Matrix([op.laguerre_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-            for idx, u in enumerate(x[-self.nU:]):
-                # Legendre polys in the first order are the constant function
-                # l^1(x)=x, perfect for the inputs
-                polys[idx + self.nSV, :] =\
-                    Matrix([op.legendre_poly(power, u)
-                            for power in indexes[idx + self.nSV, :]]).transpose()
-        else:
-            for idx, sv in enumerate(x):
-                polys[idx, :] = Matrix([op.laguerre_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-        # take the product per column
-        return Matrix([math.prod(polys[:, col])
-                       for col in range(polys.shape[1])]).transpose()
+    def assign_poly(self, orders, xsym):
+        return [
+            [op.laguerre_poly(ord, x) for ord in orders[c]] for c, x in enumerate(xsym)
+        ]
 
 
 class chebyshevtObs(pqObservable):
-    @property
-    def poly_prod(self):
-        x = symbols(f'x:{self.nSV + self.nU}')
-        indexes = self.pq_matrix
-        # create a numpy array the same size as the indexes to store
-        # the polynomials
-        polys = zeros(*indexes.shape)
-        if self.nU > 0:
-            for idx, sv in enumerate(x[:-self.nU]):
-                polys[idx, :] = Matrix([op.chebyshevt_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-            for idx, u in enumerate(x[-self.nU:]):
-                # Legendre polys in the first order are the constant function
-                # l^1(x)=x, perfect for the inputs
-                polys[idx + self.nSV, :] =\
-                    Matrix([op.legendre_poly(power, u)
-                            for power in indexes[idx + self.nSV, :]]).transpose()
-        else:
-            for idx, sv in enumerate(x):
-                polys[idx, :] = Matrix([op.chebyshevt_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-        # take the product per column
-        return Matrix([math.prod(polys[:, col])
-                       for col in range(polys.shape[1])]).transpose()
+    def assign_poly(self, orders, xsym):
+        return [
+            [op.chebyshevt_poly(ord, x) for ord in orders[c]]
+            for c, x in enumerate(xsym)
+        ]
 
 
 class chebyshevuObs(pqObservable):
-    @property
-    def poly_prod(self):
-        x = symbols(f'x:{self.nSV + self.nU}')
-        indexes = self.pq_matrix
-        # create a numpy array the same size as the indexes to store
-        # the polynomials
-        polys = zeros(*indexes.shape)
-        if self.nU > 0:
-            for idx, sv in enumerate(x[:-self.nU]):
-                polys[idx, :] = Matrix([op.chebyshevu_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-            for idx, u in enumerate(x[-self.nU:]):
-                # Legendre polys in the first order are the constant function
-                # l^1(x)=x, perfect for the inputs
-                polys[idx + self.nSV, :] =\
-                    Matrix([op.legendre_poly(power, u)
-                            for power in indexes[idx + self.nSV, :]]).transpose()
-        else:
-            for idx, sv in enumerate(x):
-                polys[idx, :] = Matrix([op.chebyshevu_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-        # take the product per column
-        return Matrix([math.prod(polys[:, col])
-                       for col in range(polys.shape[1])]).transpose()
+    def assign_poly(self, orders, xsym):
+        return [
+            [op.chebyshevu_poly(ord, x) for ord in orders[c]]
+            for c, x in enumerate(xsym)
+        ]
 
 
 class legendreObs(pqObservable):
-    @property
-    def poly_prod(self):
-        x = symbols(f'x:{self.nSV + self.nU}')
-        indexes = self.pq_matrix
-        # create a numpy array the same size as the indexes to store
-        # the polynomials
-        polys = zeros(*indexes.shape)
-        if self.nU > 0:
-            for idx, sv in enumerate(x[:-self.nU]):
-                polys[idx, :] = Matrix([op.legendre_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-            for idx, u in enumerate(x[-self.nU:]):
-                # Legendre polys in the first order are the constant function
-                # l^1(x)=x, perfect for the inputs
-                polys[idx + self.nSV, :] =\
-                    Matrix([op.legendre_poly(power, u)
-                            for power in indexes[idx + self.nSV, :]]).transpose()
-        else:
-            for idx, sv in enumerate(x):
-                polys[idx, :] = Matrix([op.legendre_poly(power, sv)
-                                        for power in indexes[idx, :]]).transpose()
-
-        # take the product per column
-        return Matrix([math.prod(polys[:, col])
-                       for col in range(polys.shape[1])]).transpose()
+    def assign_poly(self, orders, xsym):
+        return [
+            [op.legendre_poly(ord, x) for ord in orders[c]] for c, x in enumerate(xsym)
+        ]
 
 
 if __name__ == "__main__":
-    nSV = 3
-    nU = 0
-    obs = hermiteObs(nSV=nSV, p=2, q=0.9, nU=nU)
-    obs_fun = obs.pq_function
-    xx = obs_fun(3, 5, 6)
+    # obs = hermiteObs(l=3, p=2, q=0.9)
+    obs = pqObservable(l=4, p=3, q=1.2)
+    obs_function = obs.obs_fun()
+    xx = obs_function(3, 5, 6, 1)
     print(xx)
-    rng = np.random.default_rng()
-    obs.r_trx = np.triu(rng.random((7, 7)))
-    obs_fun = obs.pq_function
-    xx = obs_fun(3, 5, 6)
-    print(xx)
+    hobs = hermiteObs(l=4, p=4, q=1.2)
+    hermite_obs = hobs.obs_fun()
+    hx = hermite_obs(4, 6, 5, 6)
+    print(hx)
+    lobs = laguerreObs(l=4, p=4, q=1.2)
+    lag_obs = lobs.obs_fun()
+    lx = lag_obs(4, 6, 5, 6)
+    print(lx)
+    leobs = legendreObs(l=4, p=4, q=0.8)
+    leg_obs = leobs.obs_fun()
+    lex = lag_obs(4, 6, 5, 6)
+    print(lex)
+    x = 1
